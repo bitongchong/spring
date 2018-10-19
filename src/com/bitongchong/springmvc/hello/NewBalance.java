1a934296207c9b2970031bba007ffa1c78411e99
Administrator <Administrator@Windows2012R2>
2018-10-19 21:52:07 +0800

    2

--- src/com/bitongchong/springmvc/hello/NewBalance.java
+++ src/com/bitongchong/springmvc/hello/NewBalance.java
@@ -1,8 +1,6 @@
 package com.bitongchong.springmvc.hello;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
